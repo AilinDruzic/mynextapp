8688ba8ff49963feda5fdee7b5f3e92971bf9295
AilinDruzic <ailindruzic@gmail.com>
2023-04-12 14:46:49 +0200

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ul className="flex gap-2">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
